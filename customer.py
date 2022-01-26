c1f474d9d87c759188abe1c94709893aea8acaaf
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-01-26 22:05:28 +0900

    third commit
    応用課題完了(~C-8)

--- customer.py
+++ customer.py
@@ -8,28 +8,53 @@ class Customer:
         return f"{self.first_name} {self.family_name}"
 
     def entry_fee(self):
-        if self.age < 20:
-            return "1000"
-        elif 20 <= self.age < 65:
+        if 20 <= self.age < 65:
             return "1500"
-        else:
+        elif 65 <= self.age < 75:
             return "1200"
+        elif 3 < self.age < 20:
+            return "1000"
+        elif self.age >= 75:
+            return "500"  # C6
+        else:
+            return "0"  # C5
 
-    def info_csv(self):
-        return f"{self.first_name} {self.family_name}, {self.age}, {self.entry_fee()}"
+    def info_csv(self):  # C-4
+        return f"{self.full_name()}, {self.age}, {self.entry_fee()}"
+
+    def info_tab(self):  # C-7
+        return f"{self.full_name()}\t{self.age}\t{self.entry_fee()}"  # \t タブ区切り
+
+    def info_pipe(self):  # C-8
+        return f"{self.full_name()}|{self.age}|{self.entry_fee()}"
+
+    # Customerクラスのインスタンス化
 
 
-# Customerクラスのインスタンス化
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+nobunaga = Customer(first_name="Nobunaga", family_name="Oda", age=3)
+hideyoshi = Customer(first_name="Hideyoshi", family_name="Toyotomi", age=75)
 
-# C1 - C3
+# C-1 - C-3, C-5 - C-6
 print(ken.full_name(), ken.age, ken.entry_fee())
 print(tom.full_name(), tom.age, tom.entry_fee())
 print(ieyasu.full_name(), ieyasu.age, ieyasu.entry_fee())
+print(nobunaga.full_name(), nobunaga.age, nobunaga.entry_fee())
+print(hideyoshi.full_name(), hideyoshi.age, hideyoshi.entry_fee())
 
-# C4
+print()  # 改行
+
+# C-4, C-7 - C-8
 print(ken.info_csv())
 print(tom.info_csv())
 print(ieyasu.info_csv())
+print()
+print(ken.info_tab())
+print(tom.info_tab())
+print(ieyasu.info_tab())
+print()
+print(ken.info_pipe())
+print(tom.info_pipe())
+print(ieyasu.info_pipe())
